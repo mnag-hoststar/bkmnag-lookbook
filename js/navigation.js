8b5532dd6ea788ef5d0b6819e33d53c9318712a6
Eva Jutzeler <eju@hoststar.ch>
2018-10-24 16:15:09 +0200

    Setting up the template. Adding the following styling changes for: - Company Name, Menu - Feature - Typography - Table - Buttons, Social Icons - Form

---
+++ js/navigation.js
@@ -0,0 +1,33 @@
+// -------------------------------------
+// LookBook: JS > Navigation
+// -------------------------------------
+
+$(document).ready(function(){
+
+	// Toggles class open to display/hide the whole navigation
+	
+	$(".widget__extendednavigation").addClass("closed");
+
+	$(".extendednavigation__navigation-toggle").click(function() {
+		$(".widget__extendednavigation").toggleClass("open" ).toggleClass("closed");
+	});
+
+	// Togglse class open to display/hide folders
+	
+	$(".navigation-item.folder .item-name--parent").each(function() {
+		$(this).click(function() {
+				$(this).parent(".navigation-item.folder").toggleClass("open");
+		});
+	});
+
+	$(".extendednavigation__navigation-toggle").click(function() {
+		$("html").toggleClass("nav-open");
+	});
+    
+    // Hides other submenus when a main menu point with a submenu is clicked.
+    
+    $(".navigation-item").click(function(){
+        $(this).siblings(".navigation-item").removeClass("folder-open open");
+    });
+	
+});

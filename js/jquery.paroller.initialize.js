b974ad18d531f656848d480eef0777265c038634
Eva Jutzeler <eju@hoststar.ch>
2018-12-06 15:09:58 +0100

    Fixing certain issues with the template when it is uploaded.

--- js/jquery.paroller.initialize.js
+++ js/jquery.paroller.initialize.js
@@ -5,73 +5,73 @@
 // Initializes paroller.js and sets attributes for selected elements
 
 $(".gallery__gallery-item:nth-child(12n+1), [data-paroller-factor]").paroller({
-	factor: 0.1,            	// multiplier for scrolling speed and offset
+	factor: 0.05,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
 $(".gallery__gallery-item:nth-child(12n+2), [data-paroller-factor]").paroller({
-	factor: 0.3,            	// multiplier for scrolling speed and offset
+	factor: 0.0,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
 $(".gallery__gallery-item:nth-child(12n+3), [data-paroller-factor]").paroller({
-	factor: 0.4,            	// multiplier for scrolling speed and offset
+	factor: 0.2,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
 $(".gallery__gallery-item:nth-child(12n+4), [data-paroller-factor]").paroller({
-	factor: 0.2,            	// multiplier for scrolling speed and offset
+	factor: 0.1,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
 $(".gallery__gallery-item:nth-child(12n+5), [data-paroller-factor]").paroller({
-	factor: 0.1,            	// multiplier for scrolling speed and offset
+	factor: 0.05,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
 $(".gallery__gallery-item:nth-child(12n+6), [data-paroller-factor]").paroller({
-	factor: 0.3,            	// multiplier for scrolling speed and offset
+	factor: 0.15,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
 $(".gallery__gallery-item:nth-child(12n+7), [data-paroller-factor]").paroller({
-	factor: 0.4,            	// multiplier for scrolling speed and offset
+	factor: 0.05,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
 $(".gallery__gallery-item:nth-child(12n+8), [data-paroller-factor]").paroller({
-	factor: 0.2,            	// multiplier for scrolling speed and offset
-	type: 'foreground',     	// background, foreground
-	direction: 'vertical' 		// vertical, horizontal
-});
-
-$(".gallery__gallery-item:nth-child(12n+9), [data-paroller-factor]").paroller({
 	factor: 0.1,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
+$(".gallery__gallery-item:nth-child(12n+9), [data-paroller-factor]").paroller({
+	factor: 0.05,            	// multiplier for scrolling speed and offset
+	type: 'foreground',     	// background, foreground
+	direction: 'vertical' 		// vertical, horizontal
+});
+
 $(".gallery__gallery-item:nth-child(12n+10), [data-paroller-factor]").paroller({
-	factor: 0.3,            	// multiplier for scrolling speed and offset
+	factor: 0.15,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
 $(".gallery__gallery-item:nth-child(12n+11), [data-paroller-factor]").paroller({
-	factor: 0.4,            	// multiplier for scrolling speed and offset
+	factor: 0.2,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
 
 $(".gallery__gallery-item:nth-child(12n), [data-paroller-factor]").paroller({
-	factor: 0.2,            	// multiplier for scrolling speed and offset
+	factor: 0.1,            	// multiplier for scrolling speed and offset
 	type: 'foreground',     	// background, foreground
 	direction: 'vertical' 		// vertical, horizontal
 });
